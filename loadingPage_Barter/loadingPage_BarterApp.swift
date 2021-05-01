ab5a07e0dd7698992cb5a188b7386749e751973a
Paul Callejas <pl.callejas@icloud.com>
2021-05-01 14:47:20 -0500

    Initial Commit

---
+++ loadingPage_Barter/loadingPage_BarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  loadingPage_BarterApp.swift
+//  loadingPage_Barter
+//
+//  Created by admin on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct loadingPage_BarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
