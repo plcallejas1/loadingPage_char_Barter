28f62b54a036febecbfe5dbd76366321ee2c3f65
Paul Callejas <pl.callejas@icloud.com>
2021-05-02 22:53:09 -0500

    Splash screen and chat

---
+++ loadingPage_Barter/Chat.swift
@@ -0,0 +1,22 @@
+//
+//  Chat.swift
+//  loadingPage_Barter
+//
+//  Created by admin on 5/2/21.
+//
+
+import SwiftUI
+
+struct Chat: View {
+    var body: some View {
+       Image ("chat01")
+        .resizable()
+        .frame(width: 400, height: 850, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Chat_Previews: PreviewProvider {
+    static var previews: some View {
+        Chat()
+    }
+}
--- loadingPage_Barter/ContentView.swift
+++ loadingPage_Barter/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+        Image("MAN_WOMAN_COMPOSITION")
+            .resizable()
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .frame(width: 480, height: 480)
+   
+        Image("logo")
+          .resizable()
+          .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+          .scaledToFit()
+          .frame(width: 250, height: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        
+        Image("MAN_WOMAN_COMPOSITION")
+           .resizable()
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+           .frame(width: 480, height: 480)
+
     }
 }
 
@@ -18,4 +33,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
